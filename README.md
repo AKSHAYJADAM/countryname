11e9808643147dd5e09a332205b056fa37537dbd
AKSHAYJADAM <akshaykumarsain23@gmail.com>
2023-06-01 15:37:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+<h1 style="text-align:center"><a href="https://py-piyush.github.io/country-info/">Country Info</a></h1>
+Display information of country along with weather data of the capital
+
+- Search for any country using search bar, you'll see a list of countries
+- Click on view button to see information about the country
+
+#### Live: https://py-piyush.github.io/country-info/
+
+#### Video Demo: https://youtu.be/E8Qj4tFIQEw
+
+### APIs Information
+
+- [restcountries API](https://restcountries.com/#api-endpoints-v3): To fetch information about countries.
+- [openWeatherMap API](https://openweathermap.org/api): To fetch real-time weather data of the capital.
+
+### Demo Pictures
+
+![](images/search.png)
+<br>
+![](images/country_card.png)
---
+++ script.js
@@ -0,0 +1,121 @@
+const filter = document.querySelector("#search");
+const countriesWrapper = document.querySelector(".countries-wrapper");
+const back = document.querySelector(".back-btn");
+const modal = document.querySelector(".modal");
+const API_KEY = "29494cc03bb2a1a93840b6bf9121f3aa";
+
+const populate = (countriesArr) => {
+  
+  const children = [];
+  
+  const h3 = document.createElement("h3");
+  h3.classList.add("span-all");
+  h3.textContent = "Countries Found";
+  children.push(h3);
+  countriesArr.forEach((country) => {
+    const div = document.createElement("div");
+    div.classList.add("grid-item");
+    div.innerHTML = `<img
+      class="img"
+      src="${country.flags.svg}"
+      alt=""
+      />
+      <h4 class="country-name">${country.name.common}</h4>
+      <button class="btn">View</button>`;
+    children.push(div);
+    const btn = div.lastChild;
+    btn.addEventListener("click", weatherData(country));
+  });
+  
+
+  countriesWrapper.replaceChildren(...children);
+};
+
+const errorMessage = () => {
+  const children = document.createElement("h3");
+  children.classList.add("span-all");
+  children.textContent = "No countries found";
+  countriesWrapper.replaceChildren(children);
+};
+
+const showModal = (country, data) => {
+  const info = document.querySelector(".info");
+  info.innerHTML = `<img src="${country.flags.svg}" alt="" />
+            <h1>${country.name.common}</h1>
+            <h3>Official Name: ${country.name.official}</h3>
+            <p>Capital: ${country.capital[0]}</p>
+            <p>Region: ${country.region}</p>
+            <p>Area: ${country.area.toLocaleString()} km<sup>2</sup></p>
+            <p>Population: ${country.population.toLocaleString()}</p>
+            <p>Languages: ${Object.values(country.languages).join(", ")}</p>`;
+  modal.classList.remove("show-toggle");
+  const weather = document.querySelector(".weather");
+ 
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+};
+
+const weatherData = function (country) {
+  return function curried_func(e) {
+    const capital = country.capital[0];
+    getWeather(capital)
+      .then((data) => {
+        const weather = data.weather[0];
+        const weather_main = weather.main;
+        const weather_desc = weather.description;
+        const weather_icon_url = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+        const temperature = Math.round((data.main.temp - 273.15) * 100) / 100;
+        const feels_like =
+          Math.round((data.main.feels_like - 273.15) * 100) / 100;
+        const humidity = data.main.humidity;
+        const weatherData = {
+          weather_main,
+          weather_desc,
+          weather_icon_url,
+          temperature,
+          feels_like,
+          humidity,
+        };
+        showModal(country, weatherData);
+      })
+      .catch((error) => console.log(error));
+  };
+};
+
+// API CALLS
+
+const getCountryList = async (e) => {
+  let name = e.target.value;
+  if (name === "") countriesWrapper.replaceChildren([]);
+  if (name !== "") {
+    fetch(`https://restcountries.com/v3.1/name/${name}`)
+      .then((response) => {
+        if (response.ok) return response.json();
+        return Promise.reject(response); //Fetch promises only reject with a TypeError when a network error occurs. Since 4xx and 5xx responses aren't network errors, there's nothing to catch. You'll need to throw an error yourself
+      })
+      .then((data) => populate(data))
+      .catch((error) => {
+        console.log(error.status, error.statusText);
+        errorMessage();
+        return;
+      });
+  }
+};
+
+const getWeather = (capital) => {
+  const api_key = API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}`;
+  return fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(response);
+  });
+};
+
+// EVENT LISTENERS
+
+filter.addEventListener("input", getCountryList);
+back.addEventListener("click", () => {
+  console.log("back");
+  modal.classList.add("show-toggle");
+});
